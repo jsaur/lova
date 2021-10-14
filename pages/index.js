0f355c365e4932b37d3829f53039b634cabea941
Jacob <jsaur@kiva.org>
2021-10-14 16:29:14 +0100

    Add truffle, loan contracts, and update index.js to get flow working (#1)
    * first attempt adding truffle contracts and deploying local ledger
    
    Signed-off-by: Jacob Saur <saur.jacob@gmail.com>
    
    * Get things working
    
    Signed-off-by: Jacob Saur <saur.jacob@gmail.com>
    
    * update readme
    
    Signed-off-by: Jacob Saur <saur.jacob@gmail.com>
    
    * clean up
    
    Signed-off-by: Jacob Saur <saur.jacob@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,25 @@ import React, { useEffect, useState } from 'react';
 import { useContractKit } from '@celo-tools/use-contractkit';
 import { ContractKitProvider } from '@celo-tools/use-contractkit';
 import '@celo-tools/use-contractkit/lib/styles.css';
-import LOANABI from '../build/contracts/Loan.json'; // Manually linking ABI for now
-import ERC20ABI from '../build/contracts/ERC20.json'; // Manually linking ABI for now
+import loanJson from '../truffle/build/contracts/Loan.json'; // Manually linking ABI for now
+import erc20Json from '../truffle/build/contracts/ERC20.json'; // Manually linking ABI for now
 
 function App () {
   const { connect, network, getConnectedKit} = useContractKit();
   let [account, setAccount] = useState([]);
   let [loan, setLoan] = useState([]);
   const ERC20_DECIMALS = 18;
-  const loanAddress = '0xfda9A9cba042462709E2883018c6Fe3FD34AeE90'; // Hard-coding alfajores contrcat address of now - eventually make this config
-  const cusdAddress = '0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1'; // Hard-coding cusd - eventually allow any token
-  
+
+  // TODO Move these to configs
+  // Alfajores
+  const loanAddress = '0x88c6B693B2A79B6f8F55057c8955ce4B32746450';
+  const cusdAddress = '0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1';
+  const borrowerAddress = '0x50D3E37AfC58F8eb31a115a9c1E8AA86782e1cf8'
+  // Local
+  // const loanAddress = '0x6f42BfAe79aefA1f99553770ACd24A4A037039c1';
+  // const cusdAddress = '0x10A736A7b223f1FE1050264249d1aBb975741E75';
+  // const borrowerAddress = '0x914c756d7ed05333Ce72fb7049747f2c0b28A326'
+
   let kit;
   let loanContract;
   let cusdContract;
@@ -21,20 +29,22 @@ function App () {
   // TODO figure out how to preserve these values through button clicks
   async function initLoad() {
     kit = await getConnectedKit();
-    loanContract = new kit.web3.eth.Contract(LOANABI, loanAddress);
-    cusdContract = new kit.web3.eth.Contract(ERC20ABI, cusdAddress);
+    loanContract = new kit.web3.eth.Contract(loanJson.abi, loanAddress);
+    cusdContract = new kit.web3.eth.Contract(erc20Json.abi, cusdAddress);
     const web3Accounts = await kit.web3.eth.getAccounts();
     kit.defaultAccount = web3Accounts[0];
+    console.log(network);
   }
 
   async function getAccountSummary() {
     await initLoad();
+    // TODO kit.getTotalBalance() seems to throw a "execution reverted: Unknown account" metamask error
     const totalBalance = await kit.getTotalBalance(kit.defaultAccount);
     account = {
       address: kit.defaultAccount,
-      CELO: totalBalance.CELO.shiftedBy(-ERC20_DECIMALS).toFixed(2),
-      cUSD: totalBalance.cUSD.shiftedBy(-ERC20_DECIMALS).toFixed(2),
-      cEUR: totalBalance.cEUR.shiftedBy(-ERC20_DECIMALS).toFixed(2),
+      CELO: totalBalance.CELO.shiftedBy(-ERC20_DECIMALS).toFixed(18),
+      cUSD: totalBalance.cUSD.shiftedBy(-ERC20_DECIMALS).toFixed(18),
+      cEUR: totalBalance.cEUR.shiftedBy(-ERC20_DECIMALS).toFixed(18),
     };
     setAccount(account);
   }
@@ -43,13 +53,13 @@ function App () {
     await initLoad();
     const amountRequested = await loanContract.methods.amountRequested().call();
     const amountRaised = await loanContract.methods.amountRaised().call();
-    const amountLeftToFund = await loanContract.methods.amountLeftToFund().call();
+    const amountBorrowerHasWithdraw = await loanContract.methods.amountBorrowerHasWithdraw().call();
     const amountRepaid = await loanContract.methods.amountRepaid().call();
     const amountLenderHasWithdraw = await loanContract.methods.amountLenderHasWithdraw().call();
     loan = {
       amountRequested,
       amountRaised,
-      amountLeftToFund,
+      amountBorrowerHasWithdraw,
       amountRepaid,
       amountLenderHasWithdraw,
     };
@@ -67,27 +77,43 @@ function App () {
   async function lenderLend() {
     await initLoad();
     // Hard-coding to lend 4 for now
-    const txObject = await loanContract.methods.lend(4); 
-    await kit.sendTransactionObject(txObject, { from: kit.defaultAccount })
+    const txObject = await loanContract.methods.lend(100); 
+    let tx = await kit.sendTransactionObject(txObject, { from: kit.defaultAccount });
+    let receipt = await tx.waitReceipt();
+    console.log(receipt);
+    await getAccountSummary();
+    await getContractSummary();
   }
 
   async function borrowerWithdraw() {
     await initLoad();
     const txObject = await loanContract.methods.borrowerWithdraw(); 
-    await kit.sendTransactionObject(txObject, { from: kit.defaultAccount })
+    let tx = await kit.sendTransactionObject(txObject, { from: kit.defaultAccount });
+    let receipt = await tx.waitReceipt();
+    console.log(receipt);
+    await getAccountSummary();
+    await getContractSummary();
   }
 
   async function borrowerRepay() {
     await initLoad();
     // Hard-coding to repay 2 for now
-    const txObject = await loanContract.methods.borrowerRepay(2); 
-    await kit.sendTransactionObject(txObject, { from: kit.defaultAccount })
+    const txObject = await loanContract.methods.borrowerRepay(50); 
+    let tx = await kit.sendTransactionObject(txObject, { from: kit.defaultAccount });
+    let receipt = await tx.waitReceipt();
+    console.log(receipt);
+    await getAccountSummary();
+    await getContractSummary();
   }
 
   async function lenderWithdraw() {
     await initLoad();
     const txObject = await loanContract.methods.lenderWithdraw(); 
-    await kit.sendTransactionObject(txObject, { from: kit.defaultAccount })
+    let tx = await kit.sendTransactionObject(txObject, { from: kit.defaultAccount });
+    let receipt = await tx.waitReceipt();
+    console.log(receipt);
+    await getAccountSummary();
+    await getContractSummary();
   }
 
   useEffect(() => {
@@ -116,7 +142,7 @@ function App () {
       <div>
         <div>Amount Requested: {loan.amountRequested}</div>
         <div>Amount Raised: {loan.amountRaised}</div>
-        <div>Amount Left To Fund: {loan.amountLeftToFund}</div>
+        <div>Amount Borrower Has Withdrawn: {loan.amountBorrowerHasWithdraw}</div>
         <div>Amount Repaid: {loan.amountRepaid}</div>
         <div>Amount Lender Has Withdrawn: {loan.amountLenderHasWithdraw}</div>
         <div></div>
---
+++ truffle/truffle-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  networks: {
+    test: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    }
+  },
+  mocha: {
+    // timeout: 100000
+  },
+  compilers: {
+    solc: {
+      version: "^0.8.0",
+    }
+  },
+};
