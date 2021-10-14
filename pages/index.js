f0c691a4f3b3a2668bfa785b9d7e59bfe41e22e0
Jacob Saur <saur.jacob@gmail.com>
2021-10-14 16:04:00 +0100

    Get things working
    Signed-off-by: Jacob Saur <saur.jacob@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,17 @@ function App () {
   let [account, setAccount] = useState([]);
   let [loan, setLoan] = useState([]);
   const ERC20_DECIMALS = 18;
-  const loanAddress = '0x6f42BfAe79aefA1f99553770ACd24A4A037039c1'; // Hard-coding local contract - eventually make this config
-  const cusdAddress = '0x10A736A7b223f1FE1050264249d1aBb975741E75'; // Hard-coding cusd - eventually allow any token
-  
+
+  // TODO Move these to configs
+  // Alfajores
+  const loanAddress = '0x88c6B693B2A79B6f8F55057c8955ce4B32746450';
+  const cusdAddress = '0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1';
+  const borrowerAddress = '0x50D3E37AfC58F8eb31a115a9c1E8AA86782e1cf8'
+  // Local
+  // const loanAddress = '0x6f42BfAe79aefA1f99553770ACd24A4A037039c1';
+  // const cusdAddress = '0x10A736A7b223f1FE1050264249d1aBb975741E75';
+  // const borrowerAddress = '0x914c756d7ed05333Ce72fb7049747f2c0b28A326'
+
   let kit;
   let loanContract;
   let cusdContract;
@@ -30,12 +38,13 @@ function App () {
 
   async function getAccountSummary() {
     await initLoad();
+    // TODO kit.getTotalBalance() seems to throw a "execution reverted: Unknown account" metamask error
     const totalBalance = await kit.getTotalBalance(kit.defaultAccount);
     account = {
       address: kit.defaultAccount,
-      CELO: totalBalance.CELO.shiftedBy(-ERC20_DECIMALS).toFixed(2),
-      cUSD: totalBalance.cUSD.shiftedBy(-ERC20_DECIMALS).toFixed(2),
-      cEUR: totalBalance.cEUR.shiftedBy(-ERC20_DECIMALS).toFixed(2),
+      CELO: totalBalance.CELO.shiftedBy(-ERC20_DECIMALS).toFixed(18),
+      cUSD: totalBalance.cUSD.shiftedBy(-ERC20_DECIMALS).toFixed(18),
+      cEUR: totalBalance.cEUR.shiftedBy(-ERC20_DECIMALS).toFixed(18),
     };
     setAccount(account);
   }
@@ -44,13 +53,13 @@ function App () {
     await initLoad();
     const amountRequested = await loanContract.methods.amountRequested().call();
     const amountRaised = await loanContract.methods.amountRaised().call();
-    const amountLeftToFund = await loanContract.methods.amountLeftToFund().call();
+    const amountBorrowerHasWithdraw = await loanContract.methods.amountBorrowerHasWithdraw().call();
     const amountRepaid = await loanContract.methods.amountRepaid().call();
     const amountLenderHasWithdraw = await loanContract.methods.amountLenderHasWithdraw().call();
     loan = {
       amountRequested,
       amountRaised,
-      amountLeftToFund,
+      amountBorrowerHasWithdraw,
       amountRepaid,
       amountLenderHasWithdraw,
     };
@@ -69,26 +78,42 @@ function App () {
     await initLoad();
     // Hard-coding to lend 4 for now
     const txObject = await loanContract.methods.lend(100); 
-    await kit.sendTransactionObject(txObject, { from: kit.defaultAccount })
+    let tx = await kit.sendTransactionObject(txObject, { from: kit.defaultAccount });
+    let receipt = await tx.waitReceipt();
+    console.log(receipt);
+    await getAccountSummary();
+    await getContractSummary();
   }
 
   async function borrowerWithdraw() {
     await initLoad();
     const txObject = await loanContract.methods.borrowerWithdraw(); 
-    await kit.sendTransactionObject(txObject, { from: kit.defaultAccount })
+    let tx = await kit.sendTransactionObject(txObject, { from: kit.defaultAccount });
+    let receipt = await tx.waitReceipt();
+    console.log(receipt);
+    await getAccountSummary();
+    await getContractSummary();
   }
 
   async function borrowerRepay() {
     await initLoad();
     // Hard-coding to repay 2 for now
     const txObject = await loanContract.methods.borrowerRepay(50); 
-    await kit.sendTransactionObject(txObject, { from: kit.defaultAccount })
+    let tx = await kit.sendTransactionObject(txObject, { from: kit.defaultAccount });
+    let receipt = await tx.waitReceipt();
+    console.log(receipt);
+    await getAccountSummary();
+    await getContractSummary();
   }
 
   async function lenderWithdraw() {
     await initLoad();
     const txObject = await loanContract.methods.lenderWithdraw(); 
-    await kit.sendTransactionObject(txObject, { from: kit.defaultAccount })
+    let tx = await kit.sendTransactionObject(txObject, { from: kit.defaultAccount });
+    let receipt = await tx.waitReceipt();
+    console.log(receipt);
+    await getAccountSummary();
+    await getContractSummary();
   }
 
   useEffect(() => {
@@ -99,48 +124,11 @@ function App () {
     getContractSummary()
   }, [])
 
-  async function send(){
-    await initLoad();
-    // 12. Specify recipient Address
-    let anAddress = '0x914c756d7ed05333Ce72fb7049747f2c0b28A326'
-
-    // 13. Specify an amount to send
-    let amount = 100
-
-    // 14. Get the token contract wrappers    
-    //let goldtoken = await kit.contracts.getGoldToken()
-    let stabletoken = await kit.contracts.getStableToken()
-    
-    // 15. Transfer CELO and cUSD from your account to anAddress
-    // Specify cUSD as the feeCurrency when sending cUSD
-    //let celotx = await goldtoken.transfer(anAddress, amount).send({from: account.address})
-    let cUSDtx = await stabletoken.transfer(anAddress, amount).send({from: account.address, feeCurrency: stabletoken.address})
-    
-    // 16. Wait for the transactions to be processed
-    //let celoReceipt = await celotx.waitReceipt()
-    let cUSDReceipt = await cUSDtx.waitReceipt()
-    
-    // 17. Print receipts
-    //console.log('CELO Transaction receipt: %o', celoReceipt)
-    console.log('cUSD Transaction receipt: %o', cUSDReceipt)
-    
-    // 18. Get your new balances
-    //let celoBalance = await goldtoken.balanceOf(account.address)
-    let cUSDBalance = await stabletoken.balanceOf(account.address)
-    
-    // 19. Print new balance
-    //console.log(`Your new account CELO balance: ${celoBalance.toString()}`)
-    console.log(`Your new account cUSD balance: ${cUSDBalance.toString()}`)
-}
-
   return (
     <main>
       <h1>Lova</h1>
       <div>
         <button onClick={connect}>Click here to connect your wallet</button>
-      </div>
-      <div>
-        <button onClick={send}>Click here send</button>
       </div>
       <div>----</div>
       <div>
@@ -154,7 +142,7 @@ function App () {
       <div>
         <div>Amount Requested: {loan.amountRequested}</div>
         <div>Amount Raised: {loan.amountRaised}</div>
-        <div>Amount Left To Fund: {loan.amountLeftToFund}</div>
+        <div>Amount Borrower Has Withdrawn: {loan.amountBorrowerHasWithdraw}</div>
         <div>Amount Repaid: {loan.amountRepaid}</div>
         <div>Amount Lender Has Withdrawn: {loan.amountLenderHasWithdraw}</div>
         <div></div>
